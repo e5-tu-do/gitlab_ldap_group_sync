42f8cdacb416f5eebf4317c2a9d2589781e6c85d
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-03-26 18:27:15 +0100

    Group sync should be working now
    * Only tested reading from gitlab, not performing any changes

--- sync_gitlab_groups.py
+++ sync_gitlab_groups.py
@@ -5,12 +5,17 @@ from dotenv import load_dotenv
 import ldap
 import logging
 from collections import namedtuple
+import sys
 
 
 MOCK = not os.getenv('GITLAB_SYNC', False)
+if not MOCK:
+    # must be mock at the moment
+    sys.exit(1)
 
 LDAPGroup = namedtuple('LDAPGroup', 'dn cn members')
 LDAPUser = namedtuple('LDAPUser', 'dn uid mail')
+ACCESS = gitlab.DEVELOPER_ACCESS
 
 
 log = logging.getLogger('giblab_ldap_sync')
@@ -18,19 +23,28 @@ log.setLevel(logging.INFO)
 
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter(
-    '%(asctime)s|%(levelname)8s|%(message)s',
-    datefmt='%Y-%m-%dT%H:%M:%S.%f'
+    '%(message)s',
+    datefmt='%Y-%m-%dT%H:%M:%S'
 ))
 
 logging.getLogger().addHandler(handler)
 
 
 class Mock:
-    def __getattr__(self):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    def __getattr__(self, name):
+        if name in self.kwargs:
+            return self.kwargs[name]
         return Mock()
 
     def __call__(self, *args, **kwargs):
         return Mock()
+
+    def __repr__(self):
+        return f'Mock({self.args}, {self.kwargs})'
 
 
 def ldap_connect():
@@ -46,6 +60,7 @@ def ldap_connect():
 def get_ldap_groups(ldap_conn, min_members=1):
     '''Get LDAP groups'''
     base = os.environ['LDAP_GROUP_BASE']
+    user_base = os.environ['LDAP_USER_BASE']
     log.info(f'Using group base {base}')
 
     group_filter = os.getenv('LDAP_GROUP_FILTER')
@@ -58,11 +73,18 @@ def get_ldap_groups(ldap_conn, min_members=1):
 
     log.info(f'Found {len(result)} ldap groups')
 
-    groups = [
-        LDAPGroup(cn=g['cn'][0].decode('utf-8'), dn=dn, members=g.get('member', []))
+    groups = {
+        dn: LDAPGroup(
+            cn=g['cn'][0].decode('utf-8'),
+            dn=dn,
+            members=[
+                m.decode('utf-8')
+                for m in g.get('member', [])
+                if user_base in m.decode('utf-8')]
+        )
         for dn, g in result
         if g.get('cn') and len(g.get('member', [])) > min_members
-    ]
+    }
     log.info(f'Found {len(groups)} ldap groups with atleast {min_members} members')
 
     return groups
@@ -82,28 +104,42 @@ def get_ldap_users(ldap_conn):
     )
     log.info(f'Found {len(result)} ldap users')
 
-    users = [
-        LDAPUser(dn=dn, uid=u['uid'], mail=u['mail'])
+    users = {
+        dn: LDAPUser(
+            dn=dn,
+            uid=u['uid'][0].decode('utf-8'),
+            mail=u['mail'][0].decode('utf-8'),
+        )
         for dn, u in result
         if all([u.get('mail'), u.get('uid')])
-    ]
+    }
     log.info(f'Found {len(users)} ldap users with email and uid set')
 
     return users
 
 
 def create_group(gl, name):
-    logging.info(f'Creating gitlab group {name}')
+    log.info(f'Creating gitlab group {name}')
     if MOCK:
-        return Mock(name)
+        return Mock(name=name)
     else:
         return gl.groups.create({'name': name, 'path': name})
 
 
-def add_member(group, username, access_level):
-    logging.info(f'Adding {username} as {access_level} to group {group}')
+def add_member(group, username, access_level, gl_users):
+    if username not in gl_users:
+        log.info(f'Skipping user {username} because he/she never logged into gitlab')
+        return
+
+    log.info(f'Adding {username} to group {group.name}')
     if not MOCK:
         group.members.create(dict(user_id=username, access_level=access_level))
+
+
+def remove_member(group, username):
+    log.info(f'Removing {username} from group {group.name}')
+    if not MOCK:
+        group.members.delete(dict(member_id=username))
 
 
 def get_or_create_group(gl, name):
@@ -115,19 +151,31 @@ def get_or_create_group(gl, name):
         return group, False
     except GitlabGetError as e:
         if 'Group Not Found' in str(e):
-            log.info(f'No group {name} found in gitlab')
             return create_group(gl, name=name), True
         raise
 
 
-def sync_ldap_group(gl, ldap_group, ldap_users):
+def sync_ldap_group(gl, ldap_group, ldap_users, gl_users):
     log.info(f'Syncing LDAP group {ldap_group.dn}')
-    gitlab_group, new = get_or_create_group(ldap_group.cn)
+    gl_group, new = get_or_create_group(gl, ldap_group.cn)
 
     # if new group, we can just add all members
     if new:
+        log.info(f'Filling new group {ldap_group.cn} with ldap members')
         for member in ldap_group.members:
-            username = ldap_users[member]
+            ldap_user = ldap_users[member]
+            add_member(gl_group, ldap_user.uid, ACCESS, gl_users)
+    else:
+        gl_members = set(u.username for u in gl_group.members.list(as_list=False))
+        ldap_members = set(ldap_users[u].uid for u in ldap_group.members)
+        to_add = ldap_members - gl_members
+        to_remove = gl_members - ldap_members
+
+        for username in to_add:
+            add_member(gl_group, username, ACCESS, gl_users)
+
+        for username in to_remove:
+            remove_member(gl_group, username)
 
 
 def main():
@@ -135,10 +183,15 @@ def main():
     ldap_conn = ldap_connect()
     ldap_groups = get_ldap_groups(ldap_conn)
     ldap_users = get_ldap_users(ldap_conn)
+    ldap_conn.unbind_s()
 
     with gitlab.Gitlab(os.environ['GITLAB_URL'], os.environ['GITLAB_TOKEN']) as gl:
-        for ldap_group in ldap_groups:
-            sync_ldap_group(gl, ldap_group, ldap_users)
+
+        gl_users = {u.username: u for u in gl.users.list(as_list=False)}
+        log.info(f'Found {len(gl_users)} gitlab users')
+
+        for ldap_group in ldap_groups.values():
+            sync_ldap_group(gl, ldap_group, ldap_users, gl_users)
 
 
 if __name__ == '__main__':
